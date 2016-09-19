e79221c52b4203a184c490cd17cf0f6f42397899
Daniel Poulsen <DanP@daniels-macbook-pro-2.local>
2016-09-19 13:10:13 +0100

    Initial Commit

---
+++ UIImageColorsSwift3/UIImageColorsSwift3.h
@@ -0,0 +1,19 @@
+//
+//  UIImageColorsSwift3.h
+//  UIImageColorsSwift3
+//
+//  Created by Daniel Poulsen on 19/09/2016.
+//  Copyright © 2016 cambridgeaudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageColorsSwift3.
+FOUNDATION_EXPORT double UIImageColorsSwift3VersionNumber;
+
+//! Project version string for UIImageColorsSwift3.
+FOUNDATION_EXPORT const unsigned char UIImageColorsSwift3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageColorsSwift3/PublicHeader.h>
+
+
